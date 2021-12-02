a589d86c2e036efc3957f9d0cf6eea2d43dba049
Lilith <48443491+Stroopwafe1@users.noreply.github.com>
2021-12-02 10:53:06 +0100

    Initial Commit with day two finished

---
+++ DayOne/DayOne/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace DayOne {
+	class Program {
+		static void Main(string[] args) {
+			int output = 0;
+			string input = File.ReadAllText("./Input.txt");
+			//Console.WriteLine(input);
+			string[] parts = input.Split('\n');
+			var parsedParts = parts.Select(int.Parse).ToList();
+			for (int i = 0; i < parsedParts.Count; i++) {
+				if (i + 3 >= parsedParts.Count) {
+					break;
+				}
+
+				int groupA = parsedParts[i] + parsedParts[i + 1] + parsedParts[i + 2];
+				int groupB = parsedParts[i + 1] + parsedParts[i + 2] + parsedParts[i + 3];
+				
+				//--------- Part one -----------
+				// if (IsHigher(parsedParts[i], parsedParts[i - 1])) {
+				// 	output++;
+				// }
+				if (IsHigher(groupB, groupA))
+					output++;
+
+				//-------- Part two ------------
+			}
+
+			Console.WriteLine($"Final output: {output}");
+		}
+
+		public static bool IsHigher(int a, int b) {
+			return a > b;
+		}
+	}
+}
---
+++ DayTwo/DayTwo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace DayTwo {
+	class Program {
+		static void Main(string[] args) {
+			string input = File.ReadAllText("./input.txt");
+
+			Console.WriteLine("Part one output: " + PartOne(input));
+			Console.WriteLine("Part two output: " + PartTwo(input));
+		}
+
+		public static int PartOne(string input) {
+			int posX = 0, depth = 0;
+			string[] parts = input.Split('\n');
+			for (int i = 0; i < parts.Length; i++) {
+				string[] instructions = parts[i].Split(' ');
+				switch (instructions[0]) {
+					case "forward":
+						posX += int.Parse(instructions[1]);
+						break;
+					case "down":
+						depth += int.Parse(instructions[1]);
+						break;
+					case "up":
+						depth -= int.Parse(instructions[1]);
+						break;
+				}
+			}
+
+			return posX * depth;
+		}
+
+		public static int PartTwo(string input) {
+			int posX = 0, depth = 0, aim = 0;
+			string[] parts = input.Split('\n');
+			for (int i = 0; i < parts.Length; i++) {
+				string[] instructions = parts[i].Split(' ');
+				int amount = int.Parse(instructions[1]);
+				switch (instructions[0]) {
+					case "forward":
+						posX += amount;
+						depth += aim * amount;
+						break;
+					case "down":
+						aim += amount;
+						break;
+					case "up":
+						aim -= amount;
+						break;
+				}
+			}
+
+			return posX * depth;
+		}
+	}
+}
